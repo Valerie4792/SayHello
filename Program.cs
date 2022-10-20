42bcb43d1a5e7257d652664713f00b3743fe636d
Valerie Aguilar <val@Valeries-MBP.hsd1.ca.comcast.net>
2022-10-19 20:16:44 -0700

    Say Hello Mini Challenge added

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿//Valerie Aguilar
+//10/19/22
+
+//-------------Mini Challenge # 1 - Say Hello------------//
+
+// Directions: Create a brand new console project where you are asked your name.
+// You will then print out the following "Hello <name entered>"
+// name entered should be the variable that the user types in.
+
+//Description: 
+//identified data type, string, and assigned a variable without a value
+//Output "What is your name?"
+//assigned the variable name a value of User input
+//Have the output display "Hello" with the variable name, which is now assigned a value of user input
+
+String name;
+Console.WriteLine("What is your name?");
+name=Console.ReadLine();
+Console.WriteLine($"Hello {name}! :)");
